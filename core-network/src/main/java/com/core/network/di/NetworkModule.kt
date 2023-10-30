12e02c4fb120ab9be31c07d8092c7975f7dae942
lcz <bbccoolly@gmail.com>
2023-10-30 10:46:38 +0800

    codeStyle

--- core-network/src/main/java/com/core/network/di/NetworkModule.kt
+++ core-network/src/main/java/com/core/network/di/NetworkModule.kt
@@ -1,3 +1,19 @@
+/*
+ * Designed and developed by 2022 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.core.network.di
 
 import com.core.network.service.SunflowerApiService
@@ -30,16 +46,18 @@ internal object NetworkModule {
     @Singleton
     fun provideOkhttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().apply {
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
 //                if (BuildConfig.DEBUG) {
 //                    setLevel(HttpLoggingInterceptor.Level.BODY)
 //                } else {
 //                    setLevel(HttpLoggingInterceptor.Level.NONE)
 //                }
-            })
+                }
+            )
             .connectTimeout(20, TimeUnit.SECONDS) // 设置连接超时时间为30秒
-            .readTimeout(20, TimeUnit.SECONDS)    // 设置读取超时时间为30秒
-            .writeTimeout(20, TimeUnit.SECONDS)   // 设置写入超时时间为30秒
+            .readTimeout(20, TimeUnit.SECONDS) // 设置读取超时时间为30秒
+            .writeTimeout(20, TimeUnit.SECONDS) // 设置写入超时时间为30秒
             .build()
     }
 
@@ -66,7 +84,6 @@ internal object NetworkModule {
     fun provideSunflowerDexClient(sunflowerApiService: SunflowerApiService): SunflowerDexClient {
         return SunflowerDexClient(sunflowerApiService)
     }
-
 
     @Singleton
     @Provides
